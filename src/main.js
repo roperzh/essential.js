074ef9d406e65b062eb8184c521961457f358409
Roberto Dip <dip.jesusr@gmail.com>
2014-04-13 18:31:11 -0300

    Improves the Essential object initialization

--- src/main.js
+++ src/main.js
@@ -3,4 +3,12 @@
 // A basic object defined globally, who acts as a namespace or module
 // to hold the library
 
-Essential = window.Essential || {};
+window.Essential = {
+
+  // Essential.Core holds all the methods related to the internal working
+  // of the plugin
+  Core: {},
+
+  // Essential.Behaviors holds all the behaviors to be loaded and executed
+  Behaviors: {}
+};
--- test/main_test.js
+++ test/main_test.js
@@ -1,5 +1,15 @@
 describe("Main initializer", function() {
   it("must initialize the Essential main object", function() {
-    expect(Essential).to.not.equal(undefined);
+    expect(Essential).to.not.be.equal(undefined);
+  });
+
+  describe("Essential", function() {
+    it("must initialize the Core namespace", function() {
+      expect(Essential.Core).to.not.be.equal(undefined);
+    });
+
+    it("must initialize the Behaviors namespace", function() {
+      expect(Essential.Behaviors).to.not.be.equal(undefined);
+    });
   });
 });
