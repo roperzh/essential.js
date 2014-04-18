f1b826688324aef89b5d69d2a69751d01321eed6
Roberto Dip <dip.jesusr@gmail.com>
2014-04-18 12:10:36 -0300

    Improves the bind function, allowing to work on old browsers (IE8-)

--- src/bind.js
+++ src/bind.js
@@ -1,7 +1,13 @@
 Essential.Core.bind = function (eventName, callback, nodeList) {
   var i = -1;
 
-  while (nodeList[i++]) {
-    nodeList[i].addEventListener(eventName, callback);
+  while (nodeList[++i]) {
+    var currentElement = nodeList[i];
+
+    if (currentElement.addEventListener) {
+      nodeList[i].addEventListener(eventName, callback);
+    } else {
+      currentElement.attachEvent("on" + eventName, callback);
+    }
   }
 };
