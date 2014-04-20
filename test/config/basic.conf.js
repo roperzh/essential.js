60fc2854f51cd9715445b497e5ac4d16108e24e4
Roberto Dip <dip.jesusr@gmail.com>
2014-04-20 20:59:36 -0300

    Improves the local reports, to show coverage results in the console

--- test/config/basic.conf.js
+++ test/config/basic.conf.js
@@ -1,5 +1,20 @@
 var sharedConfig = require('./shared.conf');
 
-module.exports = function(config) {
+module.exports = function (config) {
   sharedConfig(config);
+
+  config.set({
+    coverageReporter: {
+      reporters: [
+        {
+          type: 'lcov',
+          dir: 'test/coverage/'
+        },
+        {
+          type: 'text',
+          dir: 'test/coverage'
+        }
+      ]
+    }
+  })
 };
