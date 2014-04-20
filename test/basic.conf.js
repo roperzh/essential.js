52676619bc03af9eb171871219a72daa00e1f700
Roberto Dip <dip.jesusr@gmail.com>
2014-04-19 21:53:16 -0300

    Adds coverage reports on development

--- test/basic.conf.js
+++ test/basic.conf.js
@@ -30,15 +30,21 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.html': ['html2js']
+      '**/*.html':   ['html2js'],
+      '../src/*.js': 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots'],
+    reporters: ['dots', 'coverage'],
 
+
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
 
     // web server port
     port: 9876,
